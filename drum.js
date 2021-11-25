b8c9d6cc91dbb4a2a07eebd5e1d761c0162b0666
Borubhadra Yaswanth <yaswanthborubhadra25@gmail.com>
2021-11-24 23:13:34 -0800

    initial commit

---
+++ drum.js
@@ -0,0 +1,6 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      alert("button is clicked");
+    });
+}
